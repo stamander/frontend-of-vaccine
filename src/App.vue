cedb8830ee0f8de45e02bb648f6fc8eacd43f0b2
atsushi.iida <atsushi.iida@f-code.co.jp>
2023-10-13 15:57:50 +0900

    create frame  for todo app
    Todoアプリの雛形の作成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import practice1 from './components/practice1.vue';
-let user = ref({
-firstName: 'Taro',
-lastName: 'Sato',
-});
+
+import Todo from './components/Todo.vue';
+
 </script>
 
 <template>
 
-  <practice1  :user = "user" />
-  親コンポーネントのprops の値: {{ user.firstName }}
+  <Todo />
+
 </template>
 
 <style scoped>
---
+++ src/components/Todo.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<template>
+  <h1>My ToDo App</h1>
+  <input type="text" /><button>追加</button><button>完了済みを削除する</button>
+  <ul>
+    <li>
+      <input type="checkbox" checked /><span class="todo-done"
+        >Vueをマスターする</span
+      >
+    </li>
+    <li><input type="checkbox" /><span>牛乳を買う</span></li>
+    <li><input type="checkbox" /><span>家賃を払う</span></li>
+  </ul>
+</template>
+
+<style>
+body {
+  background-color: #eee;
+}
+
+.todo-done {
+  text-decoration: line-through;
+}
+</style>
