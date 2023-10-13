bfa04b63e6f2422c76ade4b5b33771be0486aeb9
atsushi.iida <atsushi.iida@f-code.co.jp>
2023-10-13 16:57:20 +0900

    done todo app
    todoアプリの完成

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,23 +1,52 @@
-<script lang="ts">
+<script lang = "ts">
+
+// Todoアイテムに型を定義
+interface TodoItem  {
+  isDone: boolean;
+  text: string;
+}
+
 export default {
   data() {
-    return {}
+    console.log("data" + this.ToDos)
+    return {
+      newTodoText: '',
+      ToDos: [
+        // { isDone: false, text: 'ToDoの文字列'}
+      ] as TodoItem[],
+    }
   },
-  methods: {},
+  methods: {
+    addTodo() {
+      if (!this.newTodoText) return alert('文字を入力してください')
+      console.log(this.newTodoText + "newTodoText")
+      this.ToDos.push({
+        isDone: false,
+        text: this.newTodoText,
+      })
+      console.log(this.ToDos + "ToDos")
+      this.newTodoText = ''
+    },
+    clearDoneToDos() {
+      this.ToDos = this.ToDos.filter((todo) => !todo.isDone)
+    },
+  },
 }
 </script>
 
 <template>
   <h1>My ToDo App</h1>
-  <input type="text" /><button>追加</button><button>完了済みを削除する</button>
-  <ul>
-    <li>
-      <input type="checkbox" checked /><span class="todo-done"
-        >Vueをマスターする</span
+  <input type="text" v-model="newTodoText" /><button @click="addTodo">
+    追加</button
+  ><button @click="clearDoneToDos">完了済みを削除する</button>
+  <p v-if="ToDos.length === 0">ToDoがまだありません!</p>
+  <ul v-else>
+    <li v-for="ToDo in ToDos">
+      <input type="checkbox" v-model="ToDo.isDone" /><span
+        :class="{ 'todo-done': ToDo.isDone }"
+        >{{ ToDo.text }}</span
       >
     </li>
-    <li><input type="checkbox" /><span>牛乳を買う</span></li>
-    <li><input type="checkbox" /><span>家賃を払う</span></li>
   </ul>
 </template>
 
